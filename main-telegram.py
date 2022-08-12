0aa37ea59b6e31e2509efe74cb4417a5a6caa1ef
tbaiguzhinov <tim.baiguzhinov@gmail.com>
2022-08-12 10:12:39 +0600

    add VK bot

---
+++ main-telegram.py
@@ -0,0 +1,70 @@
+import logging
+import os
+
+import telegram
+from dotenv import load_dotenv
+from google.cloud import dialogflow
+from telegram import Update
+from telegram.ext import (CallbackContext, CommandHandler, Filters,
+                          MessageHandler, Updater)
+
+logger = logging.getLogger('Logger')
+
+class TelegramLogsHandler(logging.Handler):
+    """Logger handler class."""
+
+    def __init__(self, tg_bot, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.tg_bot = tg_bot
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
+
+
+def start(update: Update, context: CallbackContext):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Здравствуйте!")
+
+
+def echo(update: Update, context: CallbackContext):
+    response = get_workflow_response(update.message.text, update.message.from_user.id)
+    update.message.reply_text(response)
+
+
+def get_workflow_response(message, session_id):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+    text_input = dialogflow.TextInput(text=message, language_code='ru')
+    query_input = dialogflow.QueryInput(text=text_input)
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+    return response.query_result.fulfillment_text
+
+
+def main():
+    logger_bot = telegram.Bot(logger_bot_token)
+    logger.addHandler(TelegramLogsHandler(logger_bot, chat_id))
+    logger.warning("Саппорт бот запущен")
+
+    updater = Updater(token=telegram_token, use_context=True)
+    dispatcher = updater.dispatcher
+
+    start_handler = CommandHandler('start', start)
+    echo_handler = MessageHandler(Filters.text, echo)
+    
+    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(echo_handler)
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == "__main__":
+    load_dotenv()
+    telegram_token = os.getenv('TELEGRAM_BOT_TOKEN')
+    logger_bot_token = os.getenv('LOGGER_BOT_TOKEN')
+    chat_id = os.getenv('TELEGRAM_CHAT_ID')
+    project_id = os.getenv('GOOGLE_PROJECT_ID')
+
+    main()
--- main.py
+++ main.py
@@ -1,70 +1,25 @@
-import logging
-import os
+from json import load
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
 
-import telegram
 from dotenv import load_dotenv
-from google.cloud import dialogflow
-from telegram import Update
-from telegram.ext import (CallbackContext, CommandHandler, Filters,
-                          MessageHandler, Updater)
-
-logger = logging.getLogger('Logger')
-
-class TelegramLogsHandler(logging.Handler):
-    """Logger handler class."""
-
-    def __init__(self, tg_bot, chat_id):
-        super().__init__()
-        self.chat_id = chat_id
-        self.tg_bot = tg_bot
-
-    def emit(self, record):
-        log_entry = self.format(record)
-        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
-
-
-def start(update: Update, context: CallbackContext):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Здравствуйте!")
-
-
-def echo(update: Update, context: CallbackContext):
-    response = get_workflow_response(update.message.text, update.message.from_user.id)
-    update.message.reply_text(response)
-
-
-def get_workflow_response(message, session_id):
-    session_client = dialogflow.SessionsClient()
-    session = session_client.session_path(project_id, session_id)
-    text_input = dialogflow.TextInput(text=message, language_code='ru')
-    query_input = dialogflow.QueryInput(text=text_input)
-    response = session_client.detect_intent(
-        request={"session": session, "query_input": query_input}
-    )
-    return response.query_result.fulfillment_text
+import os
 
 
 def main():
-    logger_bot = telegram.Bot(logger_bot_token)
-    logger.addHandler(TelegramLogsHandler(logger_bot, chat_id))
-    logger.warning("Саппорт бот запущен")
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW:
+            print('Новое сообщение:')
+            if event.to_me:
+                print('Для меня от: ', event.user_id)
+            else:
+                print('От меня для: ', event.user_id)
+            print('Текст:', event.text)
 
-    updater = Updater(token=telegram_token, use_context=True)
-    dispatcher = updater.dispatcher
-
-    start_handler = CommandHandler('start', start)
-    echo_handler = MessageHandler(Filters.text, echo)
-    
-    dispatcher.add_handler(start_handler)
-    dispatcher.add_handler(echo_handler)
-
-    updater.start_polling()
-    updater.idle()
 
 if __name__ == "__main__":
     load_dotenv()
-    telegram_token = os.getenv('TELEGRAM_BOT_TOKEN')
-    logger_bot_token = os.getenv('LOGGER_BOT_TOKEN')
-    chat_id = os.getenv('TELEGRAM_CHAT_ID')
-    project_id = os.getenv('GOOGLE_PROJECT_ID')
-
+    GROUP_TOKEN = os.getenv('VK_GROUP_TOKEN')
+    vk_session = vk_api.VkApi(token=GROUP_TOKEN)
+    longpoll = VkLongPoll(vk_session)
     main()
