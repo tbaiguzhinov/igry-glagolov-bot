f1e3b0051074881a5f1bc7e4d2cc90ae6e86311f
tbaiguzhinov <tim.baiguzhinov@gmail.com>
2022-08-17 14:10:03 +0300

    Remove unnecessary ifs

--- dialogflow_response.py
+++ dialogflow_response.py
@@ -1,7 +1,6 @@
 from google.cloud import dialogflow
 
 def get_workflow_response(message, session_id, project_id):
-    is_fallback = False
     session_client = dialogflow.SessionsClient()
     session = session_client.session_path(project_id, session_id)
     text_input = dialogflow.TextInput(text=message, language_code='ru')
@@ -9,6 +8,4 @@ def get_workflow_response(message, session_id, project_id):
     response = session_client.detect_intent(
         request={"session": session, "query_input": query_input}
     )
-    if response.query_result.intent.is_fallback:
-        is_fallback = True
-    return is_fallback, response.query_result.fulfillment_text
+    return response.query_result.intent.is_fallback, response.query_result.fulfillment_text
